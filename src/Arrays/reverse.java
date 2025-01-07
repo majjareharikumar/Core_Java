833e52c6cac4d0943023a2510e323437f90a3ba8
harirebel455 <79778945+harirebel455@users.noreply.github.com>
2025-01-07 15:50:25 +0530

    Reverse

---
+++ src/Arrays/reverse.java
@@ -0,0 +1,14 @@
+package Arrays;
+
+import java.util.Arrays;
+
+public class reverse {
+    public static void main(String[] args){
+        int a[]={3,5,3,7,6,8,9};
+        int b[]=new int[a.length];
+        for(int i=0;i<a.length;i++){
+            b[i]=a[a.length-1-i];
+        }
+        System.out.println(Arrays.toString(b));
+    }
+}
