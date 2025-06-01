ac136eab755e429a3e86e9875081071eda48c472
harirebel455 <79778945+harirebel455@users.noreply.github.com>
2025-06-01 17:20:15 +0530

    sorting

---
+++ src/sorting/BubbleSort.java
@@ -0,0 +1,42 @@
+package sorting;
+
+
+//Bubble Sort – Logic
+//-->Bubble Sort is a simple sorting algorithm that works by repeatedly swapping adjacent elements if they are in the wrong order. It "bubbles" the largest (or smallest) element to the end in each pass.
+//
+//Example with Numbers
+//Let's say we have this list: [5, 3, 8, 4, 2]
+//-->First Round:
+//Compare 5 and 3 → 5 is bigger → Swap → [3, 5, 8, 4, 2]
+//Compare 5 and 8 → OK
+//Compare 8 and 4 → Swap → [3, 5, 4, 8, 2]
+//Compare 8 and 2 → Swap → [3, 5, 4, 2, 8] ← 8 is now at the end
+//-->Second Round (ignore last number):
+//Compare 3 and 5 → OK
+//Compare 5 and 4 → Swap → [3, 4, 5, 2, 8]
+//Compare 5 and 2 → Swap → [3, 4, 2, 5, 8]
+//Keep going until no swaps are needed.
+
+public class BubbleSort {
+
+    static void bubbleSort(int a[]){
+        for(int i=0;i<a.length-1;i++){
+            for(int j=0;j<a.length-1-i;j++){
+                if(a[j]>a[j+1]){
+                    int t=a[j];
+                    a[j]=a[j+1];
+                    a[j+1]=t;
+                }
+            }
+        }
+    }
+
+    public static void main(String args[]){
+        int a[]={9,2,3,8,7,5};
+        bubbleSort(a);
+        for(int n:a){
+            System.out.print(n+",");
+        }
+    }
+
+}
---
+++ src/sorting/SelectionSort.java
@@ -0,0 +1,42 @@
+package sorting;
+
+//Selection sort will work based on the index swap.
+//        Here's how:
+//Selection Sort uses indexes to:
+//Track the current position (i) in the array where the next smallest element should go.
+//Search for the smallest element by checking every element from index i+1 to the end.
+//wap the element at index i with the index of the smallest element found.
+//        🔁 Example with Indexes
+//For array: [5, 3, 8, 4, 2]
+//i = 0 → check from j = 1 to 4
+//        → find min at index 4 (value 2)
+//        → swap index 0 and index 4
+//i = 1 → check from j = 2 to 4
+//        → min is at index 1 (value 3) → no swap
+//And so on...
+
+public class SelectionSort {
+    static void selectionSort(int a[]){
+        for (int i=0;i<a.length-1;i++){
+            int index=i;
+            for(int j=i+1;j<a.length-1;j++){
+                if(a[j]<a[index]){
+                    index=j;
+                }
+            }
+            if(1!=index){
+                int t=a[i];
+                a[i]=a[index];
+                a[index]=t;
+            }
+        }
+        for(int n:a){
+            System.out.print(n+",");
+        }
+    }
+
+    public static void main(String[] args) {
+        int a[]={9,3,7,4,1,8};
+        selectionSort(a);
+    }
+}
