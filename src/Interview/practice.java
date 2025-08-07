700ce2c8cc8aa6b3b0f32cff9dcaa4bf01985d5a
harirebel455 <79778945+harirebel455@users.noreply.github.com>
2025-08-07 14:55:45 +0530

    Project

--- src/Interview/practice.java
+++ src/Interview/practice.java
@@ -1,15 +1,61 @@
 package Interview;
 
-import java.time.DayOfWeek;
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
+
+import java.util.Stack;
 
 public class practice {
 
+    static class Stack {
+        int maxsize;
+        int top;
+        int[] stackArray;
+
+
+        Stack(int size) {
+            maxsize=size;
+            top=-1;
+            stackArray=new int[size];
+        }
+
+        //push
+        void push(int n){
+            if(top==maxsize-1){
+                System.out.println("Stack is full");
+            }else{}
+            stackArray[++top]=n;
+            System.out.println("Value pushed");
+        }
+        //Peek
+        void peek(){
+            if(top==-1){
+                System.out.println("Stack is empty");
+            }else{
+                System.out.println(stackArray[top]);
+            }
+        }
+        //pop
+        void pop(){
+            if(top==-1){
+                System.out.println("Stack is empty");
+            }
+            else {
+                System.out.println(stackArray[top--]);
+            }
+        }
+    }
+
     public static void main(String[] args) {
-        String dateStr="2022-01-01";
-        LocalDate localDate=LocalDate.parse(dateStr,DateTimeFormatter.ofPattern("yyyy-MM-dd"));
-        DayOfWeek dayOfWeek=localDate.getDayOfWeek();
-        System.out.println(dayOfWeek);
+        Stack stack=new Stack(5);
+
+        stack.push(3);
+        stack.push(4);
+        stack.push(6);
+        stack.push(7);
+        stack.push(8);
+
+        stack.peek();
+        stack.pop();
+
+
     }
 }
