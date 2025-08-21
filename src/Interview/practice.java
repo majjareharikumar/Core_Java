6b85fb3781f832ebc112227fd3edc3d66ff10e55
harirebel455 <79778945+harirebel455@users.noreply.github.com>
2025-08-21 10:31:47 +0530

    SingleTon

--- src/Interview/practice.java
+++ src/Interview/practice.java
@@ -1,61 +1,26 @@
 package Interview;
 
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Stack;
 
 public class practice {
+    int j=1;
+    int sum=0;
+   public void  recursion(int n){
 
-    static class Stack {
-        int maxsize;
-        int top;
-        int[] stackArray;
-
-
-        Stack(int size) {
-            maxsize=size;
-            top=-1;
-            stackArray=new int[size];
+        if(j>n){
+            System.out.println(sum);
+            return;
         }
-
-        //push
-        void push(int n){
-            if(top==maxsize-1){
-                System.out.println("Stack is full");
-            }else{}
-            stackArray[++top]=n;
-            System.out.println("Value pushed");
-        }
-        //Peek
-        void peek(){
-            if(top==-1){
-                System.out.println("Stack is empty");
-            }else{
-                System.out.println(stackArray[top]);
-            }
-        }
-        //pop
-        void pop(){
-            if(top==-1){
-                System.out.println("Stack is empty");
-            }
-            else {
-                System.out.println(stackArray[top--]);
-            }
-        }
+       sum+=j;
+        j++;
+        recursion(n);
     }
-
     public static void main(String[] args) {
-        Stack stack=new Stack(5);
-
-        stack.push(3);
-        stack.push(4);
-        stack.push(6);
-        stack.push(7);
-        stack.push(8);
-
-        stack.peek();
-        stack.pop();
-
-
+       int i=4;
+       practice p=new practice();
+       p.recursion(i);
     }
 }
